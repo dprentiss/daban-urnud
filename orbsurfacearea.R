ecb8bd6e96c044f7c6cd9dfcb19545945e96ad46
David Prentiss <dprentiss@gmail.com>
2016-07-23 17:09:39 -0400

    limit distribution by symmetry, calculate surface area from points

--- orbsurfacearea.R
+++ orbsurfacearea.R
@@ -1,19 +1,22 @@
 ro <- 0.5  #orb radius
 d <- sqrt(2) * ro  # orthogonal distance of orb centroid from axis of rotation
-rw <- 0.7444 # radius of water surface to axis of rotation, decision variable
+rw <- 0.6 # radius of water surface to axis of rotation, decision variable
 n <- 100000  # sample size
 
 # generate uniform random cartesian points on the cylinder swept by water surface
 # let x be the direction of the cylindrical axis
 
 # generate uniform cylindrical coordinates for fixed radius rw
-x <- runif(n) - ro  # uniform x coorditates
-theta <- 2*pi*runif(n) - pi  # uniform azmuthal angles -pi to pi
+x <- ro * runif(n)  # uniform x coorditates, [0, ro]
+theta <- 0.5 * pi * runif(n)  # uniform azmuthal angles -pi to pi, [0, pi/2]
 
 # convert cylindrical to cartesian coordinates
 y <- rw*cos(theta)
 z <- rw*sin(theta)
 
 cylPoints <- data.frame(x, y, z)
-surfPoints <- cylPoints[which(x^2 + y^2 + (z - ro)^2 <= ro^2),)]
+surfPoints <- cylPoints[which(x^2 + y^2 + (z - ro)^2 <= ro^2),]
 pointRatio = nrow(surfPoints)/nrow(cylPoints)
+
+cylArea = 2*ro*2*pi*rw
+surfArea = pointRatio * cylArea
