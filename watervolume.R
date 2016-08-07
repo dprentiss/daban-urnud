9eb988559e984b9d87a7572f83ea8651c109a0b0
David Prentiss <dprentiss@gmail.com>
2016-08-06 21:02:15 -0400

    begin changing area quantities to volume

--- watervolume.R
+++ watervolume.R
@@ -1,37 +1,33 @@
 ro <- 800  # orb radius
+rw <- 1264
 d <- sqrt(2) * ro  # orthogonal distance of orb centroid from axis of rotation
-#rw <- d  # radius of water surface to axis of rotation, decision variable
-n <- 10000000  # sample size
+n <- 10000  # sample size
 
 genPointSet <- function(ro, d, rw, n) {
-  # generate uniform random cartesian points on the cylinder swept by water surface
+  # generate uniform random cartesian points on the cylinder interval swept by water
   # let x be the direction of the cylindrical axis
   
-  # generate uniform cylindrical coordinates for fixed radius rw
+  # generate uniform cylindrical coordinates for radius interval rw to 'bottom' of orb
   x <- ro * runif(n)  # uniform x coorditates, [0, ro]
   theta <- pi * runif(n) - pi/2  # uniform azmuthal angles, [-pi/2, pi/2]
+  r <- (d + ro - rw) * runif(n) + rw  # uniform radii [rw, d + ro]
 
   # convert cylindrical to cartesian coordinates an populate data frame
-  y <- rw*cos(theta)
-  z <- rw*sin(theta)
+  y <- r*cos(theta)
+  z <- r*sin(theta)
   cylPoints <- data.frame(x, y, z)
   
   # test whether points are in the orb
-  surfPoints <- cylPoints[which(x^2 + y^2 + (z + d)^2 <= ro^2),]
+  volPoints <- cylPoints[which(x^2 + y^2 + (z + d)^2 <= ro^2),]
   
   # calculate the ratio of points in the orb to points out of the orb
-  pointRatio <- nrow(surfPoints)/nrow(cylPoints)
+  pointRatio <- nrow(volPoints)/nrow(cylPoints)
 
-  cylArea <- 2*ro*2*pi*rw
-  surfArea <- pointRatio * cylArea
+  cylVol <- ((d + ro)^2 - rw^2) * pi * 2 * ro
+  waterVol <- pointRatio * cylVol
   
-  return(c(rw, surfArea, n))
+  return(c(rw, waterVol, n))
 }
 
-#for (r in seq(d-ro, d + ro, by=100)) {
-#  print(genPointSet(ro, d, r, n))
-#}
+print(genPointSet(ro, d, rw, n))
 
-for (r in seq(1241, 1281, by=5)) {
-  print(genPointSet(ro, d, r, n))
-}
